9052018be6734aeb4676ad162e1b3493d618dd94
samuelBoziau <samuel.boiziau@gmail.com>
2019-01-11 11:34:48 +0100

    Connexion BDD

--- src/dbConnect.php
+++ src/dbConnect.php
@@ -1,6 +1,20 @@
 <?php
 
-class dbConnect
-{
+class DbConnect {
+    private $user= "root";
+    private $pass = "pwroot";
 
+    public function connexionBDD(){
+
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname=DBCMS', $this->user, $this->pass);
+            foreach($dbh->query('SELECT * from CMS') as $row) {
+                print_r($row);
+            }
+            $dbh = null;
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
 }
