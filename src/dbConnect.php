2f62039c6ab96d054b956c468f032fff8e449e42
samuelBoziau <samuel.boiziau@gmail.com>
2019-01-11 11:54:39 +0100

    Modif connect

--- src/dbConnect.php
+++ src/dbConnect.php
@@ -7,9 +7,10 @@ class DbConnect {
     public function connexionBDD(){
 
         try {
-            $dbh = new PDO('mysql:host=localhost;dbname=DBCMS', $this->user, $this->pass);
+            $dbh = new PDO('mysql:host=localhost;dbname=DB_CMS', $this->user, $this->pass);
             foreach($dbh->query('SELECT * from CMS') as $row) {
                 print_r($row);
+                echo($row);
             }
             $dbh = null;
         } catch (PDOException $e) {
