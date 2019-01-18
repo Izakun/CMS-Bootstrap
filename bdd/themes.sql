fd063442da3d473f3719fdca9cc1d685ca12eccb
Alan Carrer <carrer.a@outlook.fr>
2019-01-18 16:56:58 +0100

    insert themes
    Ajout du jeu d'essai de la table themes

---
+++ bdd/themes.sql
@@ -0,0 +1,1 @@
+INSERT INTO themes (name) VALUES ("cerulean"),("cosmo"),("cyborg"),("darkly"),("flatly"),("journal"),("litera"),("lumen"),("lux"),("materia"),("minty"),("pulse"),("sandstone"),("simplex"),("sketchy"),("slate"),("solar"),("spacelab"),("superhero"),("united"),("yeti");
