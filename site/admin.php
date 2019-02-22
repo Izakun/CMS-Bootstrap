0cd42e84154cf6ffada40adb910bf3f82a4a16b9
Alan Carrer <carrer.a@outlook.fr>
2019-02-22 10:46:36 +0100

    affichage liste user panel admin

--- site/admin.php
+++ site/admin.php
@@ -25,31 +25,31 @@ if(!empty($_POST) && !empty($_POST["id"])){
 ?>
 
 <div class="col-lg-4">
-	<div class="panel panel-default">
-		<div class="panel-body"><label>Gestion des comptes</label></div>
-		<div class="panel-footer centered">
-			<table class="table table-striped table-hover">
-				<thead>
-				<tr>
-					<th>Utilisateur</th>
-					<th>Supprimer</th>
-				</tr>
-				</thead>
-				<tbody class="cours">
-				<?php foreach ($admin->getAllUser() as $key => $value): ?>
-					<form action="admin.php" method="post">
-						<tr class='active'>
-							<td><input type="hidden" name="id" value="<?php echo $value["id"]; ?>"><?php echo $value["username"];?></td>
-							<td>
-								<button class='btn btn-danger btn' type="submit"><i class="fas fa-trash"></i></button>
-							</td>
-						</tr>
-					</form>
-				<?php endforeach; ?>
-				</tbody>
-			</table>
-		</div>
-	</div>
+    <div class="card border-secondary">
+        <div class="card-header">Gestion des comptes</div>
+        <div class="card-body">
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>Utilisateur</th>
+                    <th>Supprimer</th>
+                </tr>
+                </thead>
+                <tbody class="cours">
+                <?php foreach ($admin->getAllUser() as $key => $value): ?>
+                    <form action="admin.php" method="post">
+                        <tr class='active'>
+                            <td><input type="hidden" name="id" value="<?php echo $value["id"]; ?>"><?php echo $value["username"];?></td>
+                            <td>
+                                <button class='btn btn-danger btn' type="submit"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    </form>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 <?php
 require_once("templates/default/footer.php");
