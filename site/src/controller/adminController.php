40fd37383ea6ebf692b435ee820b557c7f12ac23
samuelBoziau <32366862+samuelBoziau@users.noreply.github.com>
2019-03-01 09:59:59 +0100

    Update admin.php

--- site/src/controller/adminController.php
+++ site/src/controller/adminController.php
@@ -17,7 +17,7 @@ class adminController extends dbConnect
 
 	public function upgradeUser($id){
         $db = $this->connectDb();
-        $query = $db->prepare("UPDATE USER SET admin = 1 WHERE id = ?");
+        $query = $db->prepare("UPDATE users SET admin = 1 WHERE id = ?");
         return $query->execute(array($id));
 	}
 
